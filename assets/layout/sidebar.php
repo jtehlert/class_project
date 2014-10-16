b4928b8c866ae62e6007e4d357dc5c7ca5334312
Justin Ehlert <justin.ehlert@bottlerocketapps.com>
2014-10-16 17:07:05 -0500

    Sidebar list

--- assets/layout/sidebar.php
+++ assets/layout/sidebar.php
@@ -21,13 +21,32 @@
 			</div>
 		</a>
 	</div>
-	<div id="sidebar-search">
+	<div id="sidebar-search" >
 		<form id="sidebar-search-form" method="get" action="">
 		        <input type="text" class="sidebar-search-input" name="q" size="21" maxlength="120" placeholder="Search...">
 		        <button type="submit">Search</button>
 		</form>
 	</div>
+	<div id="sidebar-title">
+		My Notebooks
+	</div>
 	<div id="sidebar-list">
-		
+		<?php
+			for($i = 0; $i < 10; $i++)
+			{
+		?>
+		<a href="">
+			<div id="sidebar-list-cell">
+				<div class="title">
+					Notebook Name
+				</div>
+				<div class="subtitle">
+					Notebook Subtitle
+				</div>
+			</div>
+		</a>
+		<?php
+			}
+		?>
 	</div>
 </div>
