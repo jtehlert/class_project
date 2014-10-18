c032291e2903d8a01222b2f775358958b2a2e2ac
Jonathan Darling <jmdarling@me.com>
2014-10-18 10:37:53 -0500

    Updates DB schema.

--- db-init.php
+++ db-init.php
@@ -53,6 +53,7 @@ $query = 'CREATE TABLE IF NOT EXISTS ' . $_DB_CLIPPINGS_TABLE_ . '(
   ORIGFILE int(11) NOT NULL,
   COORDINATES varchar(255) NOT NULL,
   NAME varchar(255),
+  SUBTITLE varchar(255),
   PRIMARY KEY (ID)
   )';
 mysqli_query($sql, $query) or die("A MySQL error has occurred.<br />Error: (" . mysqli_errno($sql) . ") " . mysqli_error($sql));
