d910a66a7d88ba6373926eb43bea18187776eae0
Justin Ehlert <justin.ehlert@bottlerocketapps.com>
2014-11-16 14:06:33 -0600

    Home Page

---
+++ home/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Clippr</title>
+
+  <link rel="stylesheet" href="../assets/css/skeleton.css">
+  <link rel="stylesheet" href="home.css">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:800,600,400' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<header>
+	<div class="container">
+		<div class="two columns">
+			<div id="logo">
+        		<img src="../assets/images/HCI-Clippr-logo_pink.png" alt="Clippr">
+      		</div>
+		</div>
+		<div class="fourteen columns">
+			<a href="../index.php">
+				<div id="login-header">
+					Login
+				</div>
+			</a>
+		</div>
+	</div>
+</header>
+
+<div id="hero">
+	<div id="hero-container">
+		<div id="hero-content-area">
+			<h2 id="explanation-blurb">The best way to manage and save your clippings</h2>
+			<a href="../index.php" id="get-started-blurb">Get Started</a>
+		</div>
+	</div>
+</div>
+
+<div id="sections">
+	<div class="container">
+		<div class="eight columns">
+			<h2>Make Clippings</h2>
+			<img src="../assets/images/home-make-clipping.png" alt="">
+			<p>Now it's easy to make clippings from your upload files. Highlight a section, drag it over and your clipping has been made.</p>
+		</div>
+		<div class="eight columns">
+			<h2>Sharing</h2>
+			<img src="../assets/images/home-shared-note.png" alt="">
+			<p>Share your clippings with ease. Select who you want to share your clipping with and they will automatically be notified.</p>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
